0ac1566e21882e82f34c98a1cdef900516ba7356
Anders0nlima <andersoncon85@gmail.com>
2025-11-09 15:32:16 -0300

    usando o cors para interagir com o front

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from "cors"
 import { prisma } from "./src/db.js"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get("/", async function(req, res) {
     try{
