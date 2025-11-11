f5c5ad0f23458a78d1954380773411e03682530e
Anders0nlima <andersoncon85@gmail.com>
2025-11-11 15:35:28 -0300

    Porta ajustada para deploy no Render

--- backend/index.js
+++ backend/index.js
@@ -80,6 +80,8 @@ app.delete("/:id", async(req, res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log(`Servidor rodando em: http://localhost:3000`)
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em: http://localhost:${PORT}`)
 })
